6fa5f6cd0b9f273284c9712be57fa856edf0de8d
Brian Lo <brian@Brians-MacBook-Pro.local>
2025-02-26 14:16:20 -0800

    redshift SELECT 1 works

---
+++ packages/malloy-db-redshift/src/redshift_connection.ts
@@ -0,0 +1,558 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// LTNOTE: we need this extension to be installed to correctly index
+//  postgres data...  We should probably do this on connection creation...
+//
+//     create extension if not exists tsm_system_rows
+//
+
+import * as crypto from 'crypto';
+import {
+  Connection,
+  ConnectionConfig,
+  MalloyQueryData,
+  PersistSQLResults,
+  PooledConnection,
+  PostgresDialect,
+  QueryData,
+  QueryDataRow,
+  QueryOptionsReader,
+  QueryRunStats,
+  RunSQLOptions,
+  SQLSourceDef,
+  TableSourceDef,
+  StreamingConnection,
+  StructDef,
+  mkArrayDef,
+} from '@malloydata/malloy';
+import {BaseConnection} from '@malloydata/malloy/connection';
+
+import {Client, Pool} from 'pg';
+import QueryStream from 'pg-query-stream';
+import {randomUUID} from 'crypto';
+import AWS from 'aws-sdk';
+import {
+  RedshiftDataClient,
+  ExecuteStatementCommand,
+  DescribeStatementCommand,
+  GetStatementResultCommand,
+} from '@aws-sdk/client-redshift-data';
+
+interface PostgresConnectionConfiguration {
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  databaseName?: string;
+  connectionString?: string;
+}
+
+type PostgresConnectionConfigurationReader =
+  | PostgresConnectionConfiguration
+  | (() => Promise<PostgresConnectionConfiguration>);
+
+const DEFAULT_PAGE_SIZE = 1000;
+const SCHEMA_PAGE_SIZE = 1000;
+
+export interface PostgresConnectionOptions
+  extends ConnectionConfig,
+    PostgresConnectionConfiguration {}
+
+export class RedshiftConnection
+  extends BaseConnection
+  implements Connection, StreamingConnection, PersistSQLResults
+{
+  public readonly name: string;
+  private queryOptionsReader: QueryOptionsReader = {};
+  private configReader: PostgresConnectionConfigurationReader = {};
+
+  private readonly dialect = new PostgresDialect();
+
+  constructor(
+    options: PostgresConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader
+  );
+  constructor(
+    name: string,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: PostgresConnectionConfigurationReader
+  );
+  constructor(
+    arg: string | PostgresConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: PostgresConnectionConfigurationReader
+  ) {
+    super();
+    if (typeof arg === 'string') {
+      this.name = arg;
+      if (configReader) {
+        this.configReader = configReader;
+      }
+    } else {
+      const {name, ...configReader} = arg;
+      this.name = name;
+      this.configReader = configReader;
+    }
+    if (queryOptionsReader) {
+      this.queryOptionsReader = queryOptionsReader;
+    }
+  }
+
+  private async readQueryConfig(): Promise<RunSQLOptions> {
+    if (this.queryOptionsReader instanceof Function) {
+      return this.queryOptionsReader();
+    } else {
+      return this.queryOptionsReader;
+    }
+  }
+
+  protected async readConfig(): Promise<PostgresConnectionConfiguration> {
+    if (this.configReader instanceof Function) {
+      return this.configReader();
+    } else {
+      return this.configReader;
+    }
+  }
+
+  get dialectName(): string {
+    return 'postgres';
+  }
+
+  public isPool(): this is PooledConnection {
+    return false;
+  }
+
+  public canPersist(): this is PersistSQLResults {
+    return true;
+  }
+
+  public canStream(): this is StreamingConnection {
+    return true;
+  }
+
+  public get supportsNesting(): boolean {
+    return true;
+  }
+
+  protected async getClient(): Promise<Client> {
+    const {
+      username: user,
+      password,
+      databaseName: database,
+      port,
+      host,
+      connectionString,
+    } = await this.readConfig();
+    return new Client({
+      user,
+      password,
+      database,
+      port,
+      host,
+      connectionString,
+    });
+  }
+
+  protected async runPostgresQuery(
+    sqlCommand: string,
+    _pageSize: number,
+    _rowIndex: number,
+    deJSON: boolean
+  ): Promise<MalloyQueryData> {
+    const client = await this.getClient();
+    await client.connect();
+    await this.connectionSetup(client);
+
+    let result = await client.query(sqlCommand);
+    if (Array.isArray(result)) {
+      result = result.pop();
+    }
+    if (deJSON) {
+      for (let i = 0; i < result.rows.length; i++) {
+        result.rows[i] = result.rows[i].row;
+      }
+    }
+    await client.end();
+    return {
+      rows: result.rows as QueryData,
+      totalRows: result.rows.length,
+    };
+  }
+
+  async fetchSelectSchema(
+    sqlRef: SQLSourceDef
+  ): Promise<SQLSourceDef | string> {
+    const structDef: SQLSourceDef = {...sqlRef, fields: []};
+    const tempTableName = `tmp${randomUUID()}`.replace(/-/g, '');
+    const infoQuery = `
+      drop table if exists ${tempTableName};
+      create temp table ${tempTableName} as SELECT * FROM (
+        ${sqlRef.selectStr}
+      ) as x where false;
+      SELECT column_name, c.data_type, e.data_type as element_type
+      FROM information_schema.columns c LEFT JOIN information_schema.element_types e
+        ON ((c.table_catalog, c.table_schema, c.table_name, 'TABLE', c.dtd_identifier)
+          = (e.object_catalog, e.object_schema, e.object_name, e.object_type, e.collection_type_identifier))
+      where table_name='${tempTableName}';
+    `;
+    try {
+      await this.schemaFromQuery(infoQuery, structDef);
+    } catch (error) {
+      return `Error fetching schema for ${sqlRef.name}: ${error}`;
+    }
+    return structDef;
+  }
+
+  private async schemaFromQuery(
+    infoQuery: string,
+    structDef: StructDef
+  ): Promise<void> {
+    const {rows, totalRows} = await this.runPostgresQuery(
+      infoQuery,
+      SCHEMA_PAGE_SIZE,
+      0,
+      false
+    );
+    if (!totalRows) {
+      throw new Error('Unable to read schema.');
+    }
+    for (const row of rows) {
+      const postgresDataType = row['data_type'] as string;
+      const name = row['column_name'] as string;
+      if (postgresDataType === 'ARRAY') {
+        const elementType = this.dialect.sqlTypeToMalloyType(
+          row['element_type'] as string
+        );
+        structDef.fields.push(mkArrayDef(elementType, name));
+      } else {
+        const malloyType = this.dialect.sqlTypeToMalloyType(postgresDataType);
+        structDef.fields.push({...malloyType, name});
+      }
+    }
+  }
+
+  async fetchTableSchema(
+    tableKey: string,
+    tablePath: string
+  ): Promise<TableSourceDef | string> {
+    const structDef: StructDef = {
+      type: 'table',
+      name: tableKey,
+      dialect: 'postgres',
+      tablePath,
+      connection: this.name,
+      fields: [],
+    };
+    const [schema, table] = tablePath.split('.');
+    if (table === undefined) {
+      return 'Default schema not yet supported in Postgres';
+    }
+    const infoQuery = `
+      SELECT column_name, c.data_type, e.data_type as element_type
+      FROM information_schema.columns c LEFT JOIN information_schema.element_types e
+        ON ((c.table_catalog, c.table_schema, c.table_name, 'TABLE', c.dtd_identifier)
+          = (e.object_catalog, e.object_schema, e.object_name, e.object_type, e.collection_type_identifier))
+        WHERE table_name = '${table}'
+          AND table_schema = '${schema}'
+    `;
+
+    try {
+      await this.schemaFromQuery(infoQuery, structDef);
+    } catch (error) {
+      return `Error fetching schema for ${tablePath}: ${error.message}`;
+    }
+    return structDef;
+  }
+
+  public async test(): Promise<void> {
+    await this.runSQL('SELECT 1');
+  }
+
+  public async connectionSetup(client: Client): Promise<void> {
+    await client.query("SET TIME ZONE 'UTC'");
+  }
+
+  public async runSQL(
+    sql: string,
+    {rowLimit}: RunSQLOptions = {},
+    rowIndex = 0
+  ): Promise<MalloyQueryData> {
+    //const config = await this.readQueryConfig();
+
+    // Redshift
+
+    // Initiate RedshiftData client
+    const client = new RedshiftDataClient({region: 'us-west-1'});
+
+    try {
+      console.log('BRIAN Executing query:', sql);
+      // Execute the SQL statement
+      const executeCommand = new ExecuteStatementCommand({
+        WorkgroupName: 'default-workgroup', // Replace with your serverless workgroup name
+        Database: 'dev', // Replace with your database name
+        SecretArn:
+          'arn:aws:secretsmanager:us-west-1:977099028464:secret:redshift-access-secret-4MyGTg',
+        Sql: 'SELECT 1', // Your SQL query
+      });
+      const executeResponse = await client.send(executeCommand);
+      const statementId = executeResponse.Id;
+      console.log('Executing statement with ID:', statementId);
+
+      // Wait for the query to complete
+      let status: string | undefined;
+      do {
+        // Pause for 1 second between status checks
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        const describeCommand = new DescribeStatementCommand({Id: statementId});
+        const statusResponse = await client.send(describeCommand);
+        status = statusResponse.Status;
+        console.log('Current status:', status);
+      } while (
+        status !== 'FINISHED' &&
+        status !== 'FAILED' &&
+        status !== 'ABORTED'
+      );
+
+      // If the query finished successfully, fetch the results
+      if (status === 'FINISHED') {
+        const resultCommand = new GetStatementResultCommand({Id: statementId});
+        const resultResponse = await client.send(resultCommand);
+        console.log('Query results:');
+        resultResponse.Records?.forEach((record, index) => {
+          console.log(`Record ${index + 1}:`, record);
+        });
+
+        console.log(
+          'BRIAN resultResponse.ColumnMetadata:',
+          resultResponse.ColumnMetadata
+        );
+
+        // rows:
+        // Redshift's resultResponse.Records format: [ [ { columnType: 1 } ] ]
+        // we need to convert to expected rows format: [ { 'columnName': 1 } ]
+        // the actual columns names are stored separately in resultResponse.ColumnMetadata
+
+        console.log('BRIAN test:', resultResponse.Records?.[0]?.[0]?.longValue);
+
+        return {
+          rows:
+            resultResponse.Records?.map(record => {
+              const row: QueryDataRow = {};
+              record.forEach((field, index) => {
+                // Extract the first non-null value (longValue, stringValue etc)
+                const value =
+                  field.longValue ??
+                  field.stringValue ??
+                  field.doubleValue ??
+                  null;
+                console.log(
+                  'BRIAN curr col: ',
+                  resultResponse.ColumnMetadata?.[index]?.name
+                );
+                let key = resultResponse.ColumnMetadata?.[index]?.name ?? '';
+                if (key === '?column?') {
+                  // this can happen on SELECT 1 when there's no obvious column
+                  key = (index + 1).toString();
+                }
+                row[key] = value;
+              });
+              return row;
+            }) ?? [],
+          totalRows: resultResponse.TotalNumRows ?? 0,
+        };
+      } else {
+        console.error('Query did not finish successfully. Status:', status);
+      }
+    } catch (error) {
+      console.error('Error executing query:', error);
+    }
+
+    return {
+      rows: [{'-1': -1}],
+      totalRows: 0,
+    };
+
+    // return this.runPostgresQuery(
+    //   sql,
+    //   rowLimit ?? config.rowLimit ?? DEFAULT_PAGE_SIZE,
+    //   rowIndex,
+    //   true
+    // );
+  }
+
+  public async *runSQLStream(
+    sqlCommand: string,
+    {rowLimit, abortSignal}: RunSQLOptions = {}
+  ): AsyncIterableIterator<QueryDataRow> {
+    const query = new QueryStream(sqlCommand);
+    const client = await this.getClient();
+    await client.connect();
+    const rowStream = client.query(query);
+    let index = 0;
+    for await (const row of rowStream) {
+      yield row.row as QueryDataRow;
+      index += 1;
+      if (
+        (rowLimit !== undefined && index >= rowLimit) ||
+        abortSignal?.aborted
+      ) {
+        query.destroy();
+        break;
+      }
+    }
+    await client.end();
+  }
+
+  public async estimateQueryCost(_: string): Promise<QueryRunStats> {
+    return {};
+  }
+
+  public async manifestTemporaryTable(sqlCommand: string): Promise<string> {
+    const hash = crypto.createHash('md5').update(sqlCommand).digest('hex');
+    const tableName = `tt${hash}`;
+
+    const cmd = `CREATE TEMPORARY TABLE IF NOT EXISTS ${tableName} AS (${sqlCommand});`;
+    // console.log(cmd);
+    await this.runPostgresQuery(cmd, 1000, 0, false);
+    return tableName;
+  }
+
+  async close(): Promise<void> {
+    return;
+  }
+}
+
+export class PooledPostgresConnection
+  extends RedshiftConnection
+  implements PooledConnection
+{
+  private _pool: Pool | undefined;
+
+  constructor(
+    options: PostgresConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader
+  );
+  constructor(
+    name: string,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: PostgresConnectionConfigurationReader
+  );
+  constructor(
+    arg: string | PostgresConnectionOptions,
+    queryOptionsReader?: QueryOptionsReader,
+    configReader?: PostgresConnectionConfigurationReader
+  ) {
+    if (typeof arg === 'string') {
+      super(arg, queryOptionsReader, configReader);
+    } else {
+      super(arg, queryOptionsReader);
+    }
+  }
+
+  public isPool(): this is PooledConnection {
+    return true;
+  }
+
+  public async drain(): Promise<void> {
+    await this._pool?.end();
+  }
+
+  async getPool(): Promise<Pool> {
+    if (!this._pool) {
+      const {
+        username: user,
+        password,
+        databaseName: database,
+        port,
+        host,
+        connectionString,
+      } = await this.readConfig();
+      this._pool = new Pool({
+        user,
+        password,
+        database,
+        port,
+        host,
+        connectionString,
+      });
+      this._pool.on('acquire', client => client.query("SET TIME ZONE 'UTC'"));
+    }
+    return this._pool;
+  }
+
+  protected async runPostgresQuery(
+    sqlCommand: string,
+    _pageSize: number,
+    _rowIndex: number,
+    deJSON: boolean
+  ): Promise<MalloyQueryData> {
+    const pool = await this.getPool();
+    let result = await pool.query(sqlCommand);
+
+    if (Array.isArray(result)) {
+      result = result.pop();
+    }
+    if (deJSON) {
+      for (let i = 0; i < result.rows.length; i++) {
+        result.rows[i] = result.rows[i].row;
+      }
+    }
+    return {
+      rows: result.rows as QueryData,
+      totalRows: result.rows.length,
+    };
+  }
+
+  public async *runSQLStream(
+    sqlCommand: string,
+    {rowLimit, abortSignal}: RunSQLOptions = {}
+  ): AsyncIterableIterator<QueryDataRow> {
+    const query = new QueryStream(sqlCommand);
+    let index = 0;
+    // This is a strange hack... `this.pool.query(query)` seems to return the wrong
+    // type. Because `query` is a `QueryStream`, the result is supposed to be a
+    // `QueryStream` as well, but it's not. So instead, we get a client and call
+    // `client.query(query)`, which does what it's supposed to.
+    const pool = await this.getPool();
+    const client = await pool.connect();
+    const resultStream: QueryStream = client.query(query);
+    for await (const row of resultStream) {
+      yield row.row as QueryDataRow;
+      index += 1;
+      if (
+        (rowLimit !== undefined && index >= rowLimit) ||
+        abortSignal?.aborted
+      ) {
+        query.destroy();
+        break;
+      }
+    }
+    client.release();
+  }
+
+  async close(): Promise<void> {
+    await this.drain();
+  }
+}
